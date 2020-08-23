5d28aa49ed192687efcaa93ce3f3ff5660eda74d
ashitaasati1608 <ashitaasati1608@gmail.com>
2020-08-23 18:50:21 +0200

    Updates

---
+++ precache-manifest.e367f0e857234d9da2e40d3ba2afa348.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6172a5fe37d025a4b6e98038f54e4714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec871916c25aa69b996b",
+    "url": "/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "92c459ec719a03ffcdf5",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "a80ba4a4f6d906d4b759",
+    "url": "/static/css/main.5151b896.chunk.css"
+  },
+  {
+    "revision": "af3b00ca1c7965ddff7f",
+    "url": "/static/js/2.b38cb272.chunk.js"
+  },
+  {
+    "revision": "ec871916c25aa69b996b",
+    "url": "/static/js/3.123a7c76.chunk.js"
+  },
+  {
+    "revision": "92c459ec719a03ffcdf5",
+    "url": "/static/js/4.cb415ee3.chunk.js"
+  },
+  {
+    "revision": "a80ba4a4f6d906d4b759",
+    "url": "/static/js/main.b05ca4a9.chunk.js"
+  },
+  {
+    "revision": "69d2ee9c0d687f5199a8",
+    "url": "/static/js/runtime-main.3b151f16.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0c1315b00bafdc0574509298f3302b17",
+    "url": "/static/media/TCS.0c1315b0.jpg"
+  },
+  {
+    "revision": "74c30c48fbc5c724b46f6339c3d907e7",
+    "url": "/static/media/ashita.74c30c48.jpeg"
+  },
+  {
+    "revision": "86a5ac32a85c5cc53d1d8d63f5ebab77",
+    "url": "/static/media/chipotle-logo.86a5ac32.png"
+  },
+  {
+    "revision": "260e3584d235e70c4625c57b6138e6b0",
+    "url": "/static/media/deloitte_logo.260e3584.webp"
+  },
+  {
+    "revision": "8e4b1afad01b73c0a03151d17bdc8dbe",
+    "url": "/static/media/intro-right.8e4b1afa.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a990580a7367bda73bb2b4aabde3079c",
+    "url": "/static/media/reward_icon.a990580a.png"
+  },
+  {
+    "revision": "ac4587871d06c52119c517e00fef8838",
+    "url": "/static/media/sa-logo.ac458787.png"
+  },
+  {
+    "revision": "c1f9993b4fce219491d533f3f358fd86",
+    "url": "/static/media/skills-right.c1f9993b.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "88556646f36a87deb50fa62baaa19bf6",
+    "url": "/static/media/waht-i-do-left.88556646.svg"
+  }
+]);
